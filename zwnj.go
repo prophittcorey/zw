01e40e291f7ef9e4d0cd4b278ba5a5b19e93748b
Corey Prophitt <git@prophitt.me>
2023-01-10 06:55:11 -0800

    Added Trim and Present implementations

--- zwnj.go
+++ zwnj.go
@@ -1,1 +1,55 @@
 package zwnj
+
+import (
+	"strings"
+)
+
+const (
+	empty = ""
+
+	// ZWSP represents zero-width space.
+	ZWSP = '\u200B'
+
+	// ZWNBSP represents zero-width no-break space.
+	ZWNBSP = '\uFEFF'
+
+	// ZWJ represents zero-width joiner.
+	ZWJ = '\u200D'
+
+	// ZWNJ represents zero-width non-joiner.
+	ZWNJ = '\u200C'
+)
+
+var (
+	replacer = strings.NewReplacer(string(ZWSP), empty, string(ZWNBSP), empty, string(ZWJ), empty, string(ZWNJ), empty)
+
+	runes = map[rune]struct{}{
+		ZWSP:   struct{}{},
+		ZWNBSP: struct{}{},
+		ZWJ:    struct{}{},
+		ZWNJ:   struct{}{},
+	}
+)
+
+// Trim removes the specified runes from the string. By default, removes
+// all zero width characters.
+func Trim(str string, rs ...rune) string {
+	if len(rs) == 0 {
+		for r := range runes {
+			rs = append(rs, r)
+		}
+	}
+
+	return replacer.Replace(str)
+}
+
+// Present returns true if the string contains any zero width character.
+func Present(str string) bool {
+	for _, r := range str {
+		if _, ok := runes[r]; ok {
+			return true
+		}
+	}
+
+	return false
+}
--- zwnj_test.go
+++ zwnj_test.go
@@ -2,5 +2,57 @@ package zwnj
 
 import "testing"
 
-func Test(t *testing.T) {
+func TestPresence(t *testing.T) {
+	given := []string{
+		"\u200Bfrom the start",
+		"at the end\u200D",
+		"in the\u200C middle",
+		"in the\uFEFFmiddle",
+	}
+
+	for _, str := range given {
+		if !Present(str) {
+			t.Fatalf("failed to detect zero width presence in %s", str)
+		}
+	}
+
+	given = []string{
+		"",
+		"just a string",
+		"\n",
+	}
+
+	for _, str := range given {
+		if Present(str) {
+			t.Fatalf("falsely detected a zero width presence in %v", str)
+		}
+	}
+}
+
+func TestTrim(t *testing.T) {
+	given := []string{
+		"",
+		"\u200Bfrom the start",
+		"at the end\u200D",
+		"in the\u200C middle",
+		"in the\uFEFFmiddle",
+		"\t\n",
+	}
+
+	expect := []string{
+		"",
+		"from the start",
+		"at the end",
+		"in the middle",
+		"in themiddle",
+		"\t\n",
+	}
+
+	for i, str := range given {
+		trimmed := Trim(str)
+
+		if trimmed != expect[i] {
+			t.Fatalf("failed to trim %s; got %s", str, trimmed)
+		}
+	}
 }
