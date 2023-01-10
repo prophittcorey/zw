5a240fe03471505c7d52a0302f571894f2978a43
Corey Prophitt <git@prophitt.me>
2023-01-10 07:44:39 -0800

    Updated tests

--- zw_test.go
+++ zw_test.go
@@ -1,6 +1,7 @@
 package zwnj
 
 import (
+	"bytes"
 	"strings"
 	"testing"
 )
@@ -32,6 +33,33 @@ func TestPresent(t *testing.T) {
 	}
 }
 
+func TestPresentBytes(t *testing.T) {
+	given := [][]byte{
+		[]byte("\u200Bfrom the start"),
+		[]byte("at the end\u200D"),
+		[]byte("in the\u200C middle"),
+		[]byte("in the\uFEFFmiddle"),
+	}
+
+	for _, bs := range given {
+		if !Present(bytes.NewReader(bs)) {
+			t.Fatalf("failed to detect zero width presence in %s", string(bs))
+		}
+	}
+
+	given = [][]byte{
+		[]byte(""),
+		[]byte("just a string"),
+		[]byte("\n"),
+	}
+
+	for _, bs := range given {
+		if Present(bytes.NewReader(bs)) {
+			t.Fatalf("falsely detected a zero width presence in %s", string(bs))
+		}
+	}
+}
+
 func TestTrim(t *testing.T) {
 	given := []string{
 		"",
@@ -56,6 +84,33 @@ func TestTrim(t *testing.T) {
 
 		if string(trimmed) != expect[i] {
 			t.Fatalf("failed to trim %s; got %s", str, trimmed)
+		}
+	}
+}
+
+func TestTrimBytes(t *testing.T) {
+	given := [][]byte{
+		[]byte(""),
+		[]byte("\u200Bfrom the start"),
+		[]byte("at the end\u200D"),
+		[]byte("in the\u200C middle"),
+		[]byte("in the\uFEFFmiddle"),
+	}
+
+	expect := [][]byte{
+		[]byte{},
+		[]byte("from the start"),
+		[]byte("at the end"),
+		[]byte("in the middle"),
+		[]byte("in themiddle"),
+		[]byte("\t\n"),
+	}
+
+	for i, bs := range given {
+		trimmed := Trim(bytes.NewReader(bs))
+
+		if !bytes.Equal(trimmed, expect[i]) {
+			t.Fatalf("failed to trim %s; got %v", string(bs), trimmed)
 		}
 	}
 }
